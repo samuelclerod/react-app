fa22d28697c58b112ab4ee84d1266f1d2d9fab18
Samuel Rodrigues <samuelclerod@gmail.com>
2020-06-06 18:09:43 -0300

    feat: final version

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/samuelclerod/react-app",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+}
+// ["@semantic-release/github", {
+//     assets: [
+//         { path: "build.zip", label: "Build" },
+//         { path: "coverage.zip", label: "Coverage" },
+//     ]
+// }],
